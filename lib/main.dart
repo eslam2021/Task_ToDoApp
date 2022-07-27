199d694ae9903abcf2c364771012af67ace2e635
Eslam Mahmoud <70212574+eslam2021@users.noreply.github.com>
2022-07-27 21:53:25 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import '/ui/theme.dart';
+import 'package:device_preview/device_preview.dart';
+import 'db/db_helper.dart';
+import 'services/theme_services.dart';
+import 'ui/pages/home_page.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBHelper.initDb();
+  await GetStorage.init();
+  runApp(DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeServices().theme,
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
