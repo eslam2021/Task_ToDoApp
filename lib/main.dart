ccc9eeed2ab30be0246914c1e284f9fb3baa3c91
Eslam Mahmoud <70212574+eslam2021@users.noreply.github.com>
2022-07-27 23:05:37 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import '/ui/theme.dart';
-import 'package:device_preview/device_preview.dart';
 import 'db/db_helper.dart';
 import 'services/theme_services.dart';
 import 'ui/pages/home_page.dart';
@@ -16,15 +15,13 @@ void main() async {
   await GetStorage.init();
   runApp(MyApp());
 }
-
+//bb
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       theme: Themes.light,
       darkTheme: Themes.dark,
       themeMode: ThemeServices().theme,
